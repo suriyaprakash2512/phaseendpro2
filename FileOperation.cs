ce5ca060ee22c8713b7cbc70c4cc68ce92a9c048
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-12-06 16:31:41 +0530

    first cmt

---
+++ FileOperation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseEndPro_2
+{
+    internal class FileOperation
+    {
+        private const string FileName = @"D:\simplilearn projects - phase1\PhaseEndPro-2\Record.txt";
+        public void AddTeacher(Teacher teacher)
+        {
+            using (StreamWriter writer = File.AppendText(FileName))
+            {
+                writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.Class},{teacher.Section}");
+            }
+        }
+
+        public List<Teacher> GetAllTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+            if (File.Exists(FileName))
+            {
+                foreach (string line in File.ReadAllLines(FileName))
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 4)
+                    {
+                        teachers.Add(new Teacher
+                        {
+                            ID = int.Parse(parts[0]),
+                            Name = parts[1],
+                            Class = parts[2],
+                            Section = parts[3]
+                        });
+                    }
+                }
+            }
+            return teachers;
+        }
+
+        public void UpdateTeacher(int id, Teacher updatedTeacher)
+        {
+            List<string> lines = new List<string>();
+            if (File.Exists(FileName))
+            {
+                foreach (string line in File.ReadAllLines(FileName))
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 4 && int.Parse(parts[0]) == id)
+                    {
+                        lines.Add($"{updatedTeacher.ID},{updatedTeacher.Name},{updatedTeacher.Class},{updatedTeacher.Section}");
+                    }
+                    else
+                    {
+                        lines.Add(line);
+                    }
+                }
+                File.WriteAllLines(FileName, lines);
+            }
+        }
+
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseEndPro_2
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+
+    }
+}
